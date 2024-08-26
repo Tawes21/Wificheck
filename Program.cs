d6a6fb38bb828e8fa4e614758f48e4d9405b40e5
The Move <The Move@DESKTOP-EIC6F24>
2024-08-26 13:34:52 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Wificheck;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string antwort = Antwort(true);
+
+    }
+
+    static string Antwort(bool status)
+    {
+        if (status)
+        {
+            string result = "Check";
+            Console.WriteLine(result);
+            return result;
+        }
+        else 
+        {
+            string result = "Not connected/no accses";
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
